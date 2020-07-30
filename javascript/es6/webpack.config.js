6e1ac78174f0b3683ff8f68e8f91b26ac368c4bf
HANA-BOOK\smile <smilecana@gmail.com>
2020-07-29 23:45:05 -0300

    [LinkedIn - Learning ECMAScript ] Finished Chapter 2

---
+++ javascript/es6/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    entry: './script.js',
+    output: { filename: 'bundle.js' },
+    module: {
+        rules: [
+            {
+                test: /\.js?/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['env']
+                }
+            }
+        ]
+    }
+};
