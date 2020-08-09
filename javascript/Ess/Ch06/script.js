08b3697d70cad93479c4afa7108cec4f0c841a0d
HANA-BOOK\smile <smilecana@gmail.com>
2020-08-09 00:21:50 -0300

    [LinkedIn -JavaScript Essential Training ] Finished  Chapter 6 2/2

--- javascript/Ess/Ch06/script.js
+++ javascript/Ess/Ch06/script.js
@@ -1,7 +1,6 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
-
 var date = new Date();
 console.log(date);
 
@@ -15,7 +14,15 @@ console.log("Hour:" + hr + "Minute: " + min + "Second:" + sec);
 let hrPosition = (hr*360/12)+(min*(360/60)/12);
 let minPosition = (min*360/60) + (sec*(360/60)/60);
 let secPosition = sec*360/60;
+console.log(sec, hrPosition , minPosition, secPosition); 
+function runTheClock() {
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+    hrPosition = hrPosition + (3/360)  
+    minPosition = minPosition + 0.1;
+    secPosition = secPosition + 6;
+
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+var interval = setInterval(runTheClock, 1000);
